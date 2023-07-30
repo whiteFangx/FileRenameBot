98151527b502876bf04f25734681ad1a0a7f8e13
Riyan [wade] <86948336+whiteFangx@users.noreply.github.com>
2023-07-30 19:25:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5436324303:AAHmj10QbHk5GWGxxsED9j3pIcjZMvjWtOQ")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 4783634))
     API_HASH = os.environ.get("API_HASH", "f6c33f46599246676f75e153b615dbbc")
     # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001745287248")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001693208095")
     # log channel
-    #LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+    #LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001693208095")
     # Get these values from my.telegram.org
-    CHAT_ID = os.environ.get("CHAT_ID", "")
+    CHAT_ID = os.environ.get("CHAT_ID", "-1001693208095")
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5844352430 1039448354 1245624597 1858995207").split())
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
@@ -25,7 +25,7 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/f816bc9128b8016376b80.jpg")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
@@ -38,5 +38,5 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "postgres://dlsijxvw:VNxd6y5j6BU8neL9LUHStN1xq5ZIMYpV@tiny.db.elephantsql.com/dlsijxvw")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://bglrqvyi:91CJdaAvuvD3dBOS88KVsfZuvsUO4t-a@heffalump.db.elephantsql.com/bglrqvyi")
     
